303f5bad1a0a60b494bfddab6736b28250c7ab94
Hyeju <Hyeju5891@gmail.com>
2021-10-16 12:04:31 -0700

    cpnt262-a1

---
+++ Javascript/wb.js
@@ -0,0 +1,3 @@
+const toggleInside = document.queryCommandIndeterm('.toggle-inside')
+
+toggleInside.addEventListener('click', function(event) toggleInside.classlist.toggle('move') body.classlistccl.toggle('string') s
